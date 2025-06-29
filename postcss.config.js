25792b341418efbc0a88fa05eba25a7f34fb2711
nicodelau <nicolas_villagesel@hotmail.com>
2025-06-29 16:00:49 +0000

    Arreglos de dependencias

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},       // ← aquí, no @tailwindcss/postcss
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
-};
+}
