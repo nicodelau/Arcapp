93ab8267b1d8b2da4a964ba79b456f50adf6739f
Nicolas De Lauretis <75554628+nicodelau@users.noreply.github.com>
2025-07-01 16:26:10 -0300

    Update TipoComprobantesPieChart.tsx

--- app/components/charts/TipoComprobantesPieChart.tsx
+++ app/components/charts/TipoComprobantesPieChart.tsx
@@ -33,7 +33,7 @@ const TipoComprobantesPieChart: React.FC<TipoComprobantesPieChartProps> = ({
             cx="50%"
             cy="50%"
             labelLine={false}
-            label={({ tipo, percent }) => `${tipo} ${(percent * 100).toFixed(0)}%`}
+            label={({ tipo, percent }) => `${tipo} ${percent ? (percent * 100).toFixed(0) : 0}%`}
             outerRadius={80}
             fill="#8884d8"
             dataKey="total"
