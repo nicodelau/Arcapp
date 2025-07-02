6ad06f85462ea7eb31b699513df0773fe349ed6b
nicodelau <nicolas_villagesel@hotmail.com>
2025-07-02 13:25:17 +0000

    ...

--- app/comprobantes/crear-comprobante/page.tsx
+++ app/comprobantes/crear-comprobante/page.tsx
@@ -1,9 +1,5 @@
 import FacturaForm from "../../components/FacturaForm";
 
 export default function CrearComprobantePage() {
-  console.log(process.env.CUIT);
-  console.log(process.env.ARCA_WS_CRT_B64);
-  console.log(process.env.ARCA_WS_KEY_B64);
-
   return <FacturaForm />;
 }
